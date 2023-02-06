8505fb7b1d0121ea69e9379081036535d9c7d086
BuildingOwner <106369213+BuildingOwner@users.noreply.github.com>
2023-02-06 14:49:15 +0900

    Delete node directory

--- node/database.js
+++
@@ -1,9 +0,0 @@
-const mysql = require("mysql2/promise");
-
-exports.pool = mysql.createPool({
-  host: "www.cjwstudy.shop",
-  user: "dummy-client",
-  port: "3306",
-  password: "chlwodhks911@",
-  database: "MyTodoDB",
-});
