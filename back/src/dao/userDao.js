e4dbbbc8456fc50e33ceb9e8fba780347fb44816
BuildingOwner <106369213+BuildingOwner@users.noreply.github.com>
2023-02-06 14:45:47 +0900

    github 업로드 파일 분리

---
+++ back/src/dao/userDao.js
@@ -0,0 +1,105 @@
+const { pool } = require("../../database");
+
+exports.insertUser = async function (email, password, nickname) {
+  try {
+    // DB 연결 검사
+    const connection = await pool.getConnection(async (conn) => conn);
+
+    try {
+      // 쿼리
+      const insertUserQuery = "insert into Users (email,password, nickname) values (?, ?, ?);"
+      const insertUserParams = [email, password, nickname];
+
+      const [row] = await connection.query(insertUserQuery, insertUserParams);
+      return row;
+
+    } catch (err) {
+      console.error(`##### insertUser Query error ##### \n ${err}`);
+      return false;
+
+    } finally {
+      connection.release();
+    }
+  } catch (err) {
+    console.error(` ##### insertUser DB error ##### \n ${err}`);
+    return false;
+  }
+};
+
+exports.selectUserByEmail = async function (email) {
+  try {
+    // DB 연결 검사
+    const connection = await pool.getConnection(async (conn) => conn);
+
+    try {
+      // 쿼리
+      const selectUserByEmailQuery = "select * from Users where email = ?";
+      const selectUserByEmailParams = [email];
+
+      const [row] = await connection.query(selectUserByEmailQuery, selectUserByEmailParams);
+      return row;
+
+    } catch (err) {
+      console.error(`##### selectUserByEmail Query error ##### \n ${err}`);
+      return false;
+
+    } finally {
+      connection.release();
+    }
+  } catch (err) {
+    console.error(` ##### selectUserByEmail DB error ##### \n ${err}`);
+    return false;
+  }
+};
+
+exports.selectUser = async function (email, password) {
+  try {
+    // DB 연결 검사
+    const connection = await pool.getConnection(async (conn) => conn);
+
+    try {
+      // 쿼리
+      const selectUserQuery = "select * from Users where email = ? and password = ?";
+      const selectUserParams = [email, password];
+
+      const [row] = await connection.query(selectUserQuery, selectUserParams);
+      return row;
+
+    } catch (err) {
+      console.error(`##### selectUser Query error ##### \n ${err}`);
+      return false;
+
+    } finally {
+      connection.release();
+    }
+  } catch (err) {
+    console.error(` ##### selectUser DB error ##### \n ${err}`);
+    return false;
+  }
+};
+
+exports.selectNicknameByUserIdx = async function (userIdx) {
+  try {
+    // DB 연결 검사
+    const connection = await pool.getConnection(async (conn) => conn);
+
+    try {
+      // 쿼리
+      const selectNicknameByUserIdxQuery = "select * from Users where userIdx = ?;";
+      const selectNicknameByUserIdxParams = [userIdx];
+
+      const [row] = await connection.query(selectNicknameByUserIdxQuery, selectNicknameByUserIdxParams);
+      return row;
+
+    } catch (err) {
+      console.error(`##### selectNicknameByUserIdx Query error ##### \n ${err}`);
+      return false;
+
+    } finally {
+      connection.release();
+    }
+  } catch (err) {
+    console.error(` ##### selectNicknameByUserIdx DB error ##### \n ${err}`);
+    return false;
+  }
+}
